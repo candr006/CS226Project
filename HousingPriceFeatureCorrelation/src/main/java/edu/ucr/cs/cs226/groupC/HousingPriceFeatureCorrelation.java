20a94c23d0ffef0407234e43797ea77f49e30638
candr006 <candr006@ucr.edu>
2018-11-27 06:14:41 +0000

    adding maven project

---
+++ HousingPriceFeatureCorrelation/src/main/java/edu/ucr/cs/cs226/groupC/HousingPriceFeatureCorrelation.java
@@ -0,0 +1,13 @@
+package edu.ucr.cs.cs226.groupC;
+
+/**
+ * Hello world!
+ *
+ */
+public class HousingPriceFeatureCorrelation
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
