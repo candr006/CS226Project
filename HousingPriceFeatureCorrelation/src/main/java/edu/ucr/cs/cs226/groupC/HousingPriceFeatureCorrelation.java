a82bab2a85770b77d5a9596610738178207c8cd5
candr006 <candr006@ucr.edu>
2018-11-28 06:51:43 +0000

    reading from input file

--- HousingPriceFeatureCorrelation/src/main/java/edu/ucr/cs/cs226/groupC/HousingPriceFeatureCorrelation.java
+++ HousingPriceFeatureCorrelation/src/main/java/edu/ucr/cs/cs226/groupC/HousingPriceFeatureCorrelation.java
@@ -1,13 +1,21 @@
 package edu.ucr.cs.cs226.groupC;
 
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
 /**
  * Hello world!
  *
  */
-public class HousingPriceFeatureCorrelation
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class HousingPriceFeatureCorrelation {
+    public static void main(String[] args) {
+// configure spark
+
+        JavaSparkContext sc = new JavaSparkContext();
+        JavaRDD<String> data = sc.textFile("boston_input_file.csv");
+        System.out.println(data.toString());
+
+        sc.stop();
     }
 }
+
