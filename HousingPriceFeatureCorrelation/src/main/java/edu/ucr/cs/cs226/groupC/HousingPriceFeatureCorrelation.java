ad284e839edf6bb60b6ca0d8d26aef6956239a61
candr006 <candr006@ucr.edu>
2018-11-29 03:37:14 +0000

    Use SparkSession to load csv into Dataset

--- HousingPriceFeatureCorrelation/src/main/java/edu/ucr/cs/cs226/groupC/HousingPriceFeatureCorrelation.java
+++ HousingPriceFeatureCorrelation/src/main/java/edu/ucr/cs/cs226/groupC/HousingPriceFeatureCorrelation.java
@@ -2,18 +2,28 @@ package edu.ucr.cs.cs226.groupC;
 
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
 
-/**
- * Hello world!
- *
- */
+
 public class HousingPriceFeatureCorrelation {
     public static void main(String[] args) {
-// configure spark
 
         JavaSparkContext sc = new JavaSparkContext();
         JavaRDD<String> data = sc.textFile("boston_input_file.csv");
-        System.out.println(data.toString());
+
+        //start the spark session
+        SparkSession spark = SparkSession
+                .builder()
+                .appName("Housing Price Feature Correlation")
+                .getOrCreate();
+
+        //Load Boston csv to dataset
+        Dataset<Row> boston_csv = spark.read().format("csv").option("header","true").load("boston_input_file.csv");
+        //output the contents of the input file
+        boston_csv.show();
+
 
         sc.stop();
     }
