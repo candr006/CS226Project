cf3da430a2df47f021046eb429f85ad0c74cb050
candr006 <candr006@ucr.edu>
2018-12-04 05:05:17 +0000

    revert pom.xml dependencies. compiles and runs with scala.Match GenericRowwithSchema error

--- HousingPriceFeatureCorrelation/src/main/java/edu/ucr/cs/cs226/groupC/HousingPriceFeatureCorrelation.java
+++ HousingPriceFeatureCorrelation/src/main/java/edu/ucr/cs/cs226/groupC/HousingPriceFeatureCorrelation.java
@@ -2,11 +2,18 @@ package edu.ucr.cs.cs226.groupC;
 
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.ml.stat.Correlation;
+import org.apache.spark.mllib.linalg.Matrix;
+import org.apache.spark.mllib.linalg.Vector;
+import org.apache.spark.rdd.RDD;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+//import org.apache.spark.mllib.stat.*;
+import org.apache.spark.sql.DataFrameStatFunctions;
+import org.apache.spark.mllib.stat.correlation.Correlation.*;
 
-//import static org.apache.spark.sql.functions.corr;
+import static org.apache.spark.mllib.stat.correlation.Correlations.corr;
 
 
 public class HousingPriceFeatureCorrelation {
@@ -23,9 +30,10 @@ public class HousingPriceFeatureCorrelation {
 
         //Load Boston csv to dataset
         Dataset<Row> boston_csv = spark.read().format("csv").option("header","true").load("boston_input_file.csv");
-
-        //Correlation Matrix
-        //Dataset<Row> corr_matrix= Correlation.corr(boston_csv, "SalePrice");
+        boston_csv= boston_csv.filter("PoolArea != ''");
+        //Correlation between columns?
+        Dataset<Row> correlated= Correlation.corr(boston_csv, "PoolArea", "pearson");
+        correlated.show();
 
 
         sc.stop();
