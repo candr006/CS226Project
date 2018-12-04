c69c9741bf788d01ad80c84dd30eb112e55a6b2d
candr006 <candr006@ucr.edu>
2018-12-04 21:58:51 +0000

    scala.Match error again after adding Metadata.empty()

--- HousingPriceFeatureCorrelation/src/main/java/edu/ucr/cs/cs226/groupC/HousingPriceFeatureCorrelation.java
+++ HousingPriceFeatureCorrelation/src/main/java/edu/ucr/cs/cs226/groupC/HousingPriceFeatureCorrelation.java
@@ -5,14 +5,15 @@ import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.ml.stat.Correlation;
 import org.apache.spark.mllib.linalg.Matrix;
 import org.apache.spark.mllib.linalg.Vector;
+import org.apache.spark.mllib.stat.Statistics;
 import org.apache.spark.rdd.RDD;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
-//import org.apache.spark.mllib.stat.*;
 import org.apache.spark.sql.DataFrameStatFunctions;
 import org.apache.spark.mllib.stat.correlation.Correlation.*;
 import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.Metadata;
 import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
 
@@ -27,95 +28,97 @@ public class HousingPriceFeatureCorrelation {
         JavaSparkContext sc = new JavaSparkContext();
         JavaRDD<String> data = sc.textFile("boston_input_file.csv");
 
+        //schema
+        StructType customSchema = customSchema = new StructType(new StructField[] {
+                new StructField("Id",DataTypes.IntegerType, true, Metadata.empty()),
+                new StructField("MSSubClass",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("MSZoning",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("LotFrontage",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("LotArea",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("Street",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("Alley",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("LotShape",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("LandContour",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("Utilities",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("LotConfig",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("LandSlope",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("Neighborhood",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("Condition1",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("Condition2",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("BldgType",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("HouseStyle",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("OverallQual",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("OverallCond",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("YearBuilt",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("YearRemodAdd",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("RoofStyle",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("RoofMatl",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("Exterior1st",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("Exterior2nd",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("MasVnrType",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("MasVnrArea",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("ExterQual",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("ExterCond",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("Foundation",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("BsmtQual",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("BsmtCond",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("BsmtExposure",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("BsmtFinType1",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("BsmtFinSF1",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("BsmtFinType2",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("BsmtFinSF2",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("BsmtUnfSF",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("TotalBsmtSF",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("Heating",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("HeatingQC",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("CentralAir",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("Electrical",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("1stFlrSF",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("2ndFlrSF",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("LowQualFinSF",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("GrLivArea",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("BsmtFullBath",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("BsmtHalfBath",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("FullBath",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("HalfBath",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("BedroomAbvGr",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("KitchenAbvGr",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("KitchenQual",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("TotRmsAbvGrd",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("Functional",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("Fireplaces",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("FireplaceQu",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("GarageType",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("GarageYrBlt",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("GarageFinish",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("GarageCars",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("GarageArea",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("GarageQual",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("GarageCond",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("PavedDrive",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("WoodDeckSF",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("OpenPorchSF",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("EnclosedPorch",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("3SsnPorch",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("ScreenPorch",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("PoolArea",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("PoolQC",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("Fence",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("MiscFeature",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("MiscVal",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("MoSold",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("YrSold",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("SaleType",DataTypes.StringType, true, Metadata.empty()),
+                new StructField("SaleCondition", DataTypes.StringType, true, Metadata.empty()),
+                new StructField("SalePrice",DataTypes.IntegerType,true, Metadata.empty())});
+
         //start the spark session
         SparkSession spark = SparkSession
                 .builder()
                 .appName("Housing Price Feature Correlation")
                 .getOrCreate();
 
-        //schema
-        StructType customSchema = customSchema = new StructType(new StructField[] {
-                new StructField("Id",DataTypes.IntegerType, true, null),
-                new StructField("MSSubClass",DataTypes.StringType, true, null),
-                new StructField("MSZoning",DataTypes.StringType, true, null),
-                new StructField("LotFrontage",DataTypes.StringType, true, null),
-                new StructField("LotArea",DataTypes.StringType, true, null),
-                new StructField("Street",DataTypes.StringType, true, null),
-                new StructField("Alley",DataTypes.StringType, true, null),
-                new StructField("LotShape",DataTypes.StringType, true, null),
-                new StructField("LandContour",DataTypes.StringType, true, null),
-                new StructField("Utilities",DataTypes.StringType, true, null),
-                new StructField("LotConfig",DataTypes.StringType, true, null),
-                new StructField("LandSlope",DataTypes.StringType, true, null),
-                new StructField("Neighborhood",DataTypes.StringType, true, null),
-                new StructField("Condition1",DataTypes.StringType, true, null),
-                new StructField("Condition2",DataTypes.StringType, true, null),
-                new StructField("BldgType",DataTypes.StringType, true, null),
-                new StructField("HouseStyle",DataTypes.StringType, true, null),
-                new StructField("OverallQual",DataTypes.StringType, true, null),
-                new StructField("OverallCond",DataTypes.StringType, true, null),
-                new StructField("YearBuilt",DataTypes.StringType, true, null),
-                new StructField("YearRemodAdd",DataTypes.StringType, true, null),
-                new StructField("RoofStyle",DataTypes.StringType, true, null),
-                new StructField("RoofMatl",DataTypes.StringType, true, null),
-                new StructField("Exterior1st",DataTypes.StringType, true, null),
-                new StructField("Exterior2nd",DataTypes.StringType, true, null),
-                new StructField("MasVnrType",DataTypes.StringType, true, null),
-                new StructField("MasVnrArea",DataTypes.StringType, true, null),
-                new StructField("ExterQual",DataTypes.StringType, true, null),
-                new StructField("ExterCond",DataTypes.StringType, true, null),
-                new StructField("Foundation",DataTypes.StringType, true, null),
-                new StructField("BsmtQual",DataTypes.StringType, true, null),
-                new StructField("BsmtCond",DataTypes.StringType, true, null),
-                new StructField("BsmtExposure",DataTypes.StringType, true, null),
-                new StructField("BsmtFinType1",DataTypes.StringType, true, null),
-                new StructField("BsmtFinSF1",DataTypes.StringType, true, null),
-                new StructField("BsmtFinType2",DataTypes.StringType, true, null),
-                new StructField("BsmtFinSF2",DataTypes.StringType, true, null),
-                new StructField("BsmtUnfSF",DataTypes.StringType, true, null),
-                new StructField("TotalBsmtSF",DataTypes.StringType, true, null),
-                new StructField("Heating",DataTypes.StringType, true, null),
-                new StructField("HeatingQC",DataTypes.StringType, true, null),
-                new StructField("CentralAir",DataTypes.StringType, true, null),
-                new StructField("Electrical",DataTypes.StringType, true, null),
-                new StructField("1stFlrSF",DataTypes.StringType, true, null),
-                new StructField("2ndFlrSF",DataTypes.StringType, true, null),
-                new StructField("LowQualFinSF",DataTypes.StringType, true, null),
-                new StructField("GrLivArea",DataTypes.StringType, true, null),
-                new StructField("BsmtFullBath",DataTypes.StringType, true, null),
-                new StructField("BsmtHalfBath",DataTypes.StringType, true, null),
-                new StructField("FullBath",DataTypes.StringType, true, null),
-                new StructField("HalfBath",DataTypes.StringType, true, null),
-                new StructField("BedroomAbvGr",DataTypes.StringType, true, null),
-                new StructField("KitchenAbvGr",DataTypes.StringType, true, null),
-                new StructField("KitchenQual",DataTypes.StringType, true, null),
-                new StructField("TotRmsAbvGrd",DataTypes.StringType, true, null),
-                new StructField("Functional",DataTypes.StringType, true, null),
-                new StructField("Fireplaces",DataTypes.StringType, true, null),
-                new StructField("FireplaceQu",DataTypes.StringType, true, null),
-                new StructField("GarageType",DataTypes.StringType, true, null),
-                new StructField("GarageYrBlt",DataTypes.StringType, true, null),
-                new StructField("GarageFinish",DataTypes.StringType, true, null),
-                new StructField("GarageCars",DataTypes.StringType, true, null),
-                new StructField("GarageArea",DataTypes.StringType, true, null),
-                new StructField("GarageQual",DataTypes.StringType, true, null),
-                new StructField("GarageCond",DataTypes.StringType, true, null),
-                new StructField("PavedDrive",DataTypes.StringType, true, null),
-                new StructField("WoodDeckSF",DataTypes.StringType, true, null),
-                new StructField("OpenPorchSF",DataTypes.StringType, true, null),
-                new StructField("EnclosedPorch",DataTypes.StringType, true, null),
-                new StructField("3SsnPorch",DataTypes.StringType, true, null),
-                new StructField("ScreenPorch",DataTypes.StringType, true, null),
-                new StructField("PoolArea",DataTypes.StringType, true, null),
-                new StructField("PoolQC",DataTypes.StringType, true, null),
-                new StructField("Fence",DataTypes.StringType, true, null),
-                new StructField("MiscFeature",DataTypes.StringType, true, null),
-                new StructField("MiscVal",DataTypes.StringType, true, null),
-                new StructField("MoSold",DataTypes.StringType, true, null),
-                new StructField("YrSold",DataTypes.StringType, true, null),
-                new StructField("SaleType",DataTypes.StringType, true, null),
-                new StructField("SaleCondition", DataTypes.StringType, true, null),
-                new StructField("SalePrice",DataTypes.IntegerType,true, null)});
+
 
         //Load Boston csv to dataset
         Dataset<Row> boston_csv = spark.read()
@@ -128,7 +131,6 @@ public class HousingPriceFeatureCorrelation {
         //Correlation between columns?
         Dataset<Row> correlated= Correlation.corr(boston_csv, "SalePrice", "pearson");
         correlated.show();
-
 
         sc.stop();
     }
